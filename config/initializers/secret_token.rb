ac7848bac1ad5ea357f26b41045b1cfb4f4bb613
Calvin Li <calvinli@mit.edu>
2014-06-22 20:58:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RunThisTown::Application.config.secret_key_base = '2569d4202108620d7dc1924ad34f70edfe906c3d8a5a18b70483ab5bd7afdda8972415593104bba6c5a5b0b4502338a254b35a85aab392b9497b9af250fed6e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RunThisTown::Application.config.session_store :cookie_store, key: '_RunThisTown_session'
