08e5df18729323ba876e99c42323765926e584f9
Calvin Li <calvinli@mit.edu>
2014-06-30 22:02:52 -0700

    Made priliminary landing page controller and setup, testing production

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RunThisTown::Application.routes.draw do
+  root "landing#home"
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
