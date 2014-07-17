27175e0a15c56077e2ebeff1ffe73961a2b4cbba
Calvin Li <calvinli@mit.edu>
2014-07-16 22:26:09 -0700

    updated registration modal and registration page to get the correct params to make user

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,13 @@
 class LandingController < ApplicationController
+  before_filter :logged_in
+
   def home
+  end
+
+  private
+  def logged_in
+  	if current_user != nil
+  	  redirect_to user_show_path
+  	end
   end
 end
