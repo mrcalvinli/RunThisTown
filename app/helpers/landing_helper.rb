9a8e4f9c5d141f06671e308951462a7be70c5c1c
Calvin Li <calvinli@mit.edu>
2014-07-19 19:40:18 -0700

    displays name of user on homepage, corrected sign up form to get last name

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -2,6 +2,6 @@ module LandingHelper
 
   # Date options for the sign up module
   def date_options
-    { :start_year => Date.today.year - 100 }
+    return { :start_year => Date.today.year - 100 }
   end
 end
