ada51ae9d920f337627c8edcaf5ec5c7ced7ccaf
Calvin Li <calvinli@mit.edu>
2014-07-21 23:30:25 -0700

    added relationship, displays on homepage

--- app/models/run_route.rb
+++ app/models/run_route.rb
@@ -1,4 +1,8 @@
 class RunRoute < ActiveRecord::Base
+	#Relationship
+	belongs_to :user
+
+	#Serializing text into array for Locations in mysql
 	serialize :locations, Array
 
 	#The data structure that represents a location in the array of locations in route
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+	#Relationships
+	has_many :run_routes
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, 
